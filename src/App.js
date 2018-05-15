f0aeca8d27983d1391309758dec69b3a0561e394
antoniocarreiro <up201404828@fe.up.pt>
2018-05-16 00:38:43 +0100

    vambora

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import { Howl } from 'howler';
 import React, { Component } from 'react';
-import './App.css';
-import Slider from './Slider';
-import {Howl} from 'howler';
 import Rodal from 'rodal';
 import 'rodal/lib/rodal.css';
-
+import Slider from './Slider';
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -12,185 +11,189 @@ class App extends Component {
     this.state = {
       sliders: [
         {
-          title: "Drums", volume: Math.random()
+          title: 'Drums',
+          volume: Math.random(),
         },
 
         {
-          title: "Vocals", volume: Math.random()
+          title: 'Vocals',
+          volume: Math.random(),
         },
 
         {
-          title: "Lead", volume: Math.random()
+          title: 'Lead',
+          volume: Math.random(),
         },
 
         {
-          title: "Bass", volume: Math.random()
-        }
+          title: 'Bass',
+          volume: Math.random(),
+        },
       ],
 
       toggle: true,
-      submitWindow: false
-    }
+      submitWindow: false,
+    };
 
     this.userSounds = [
       {
-        sound: new Howl({ src: ['atw.mp3']}),
+        sound: new Howl({ src: ['atw.mp3'] }),
       },
 
       {
-        sound: new Howl({ src: ['dafunk.mp3']}),
-
+        sound: new Howl({ src: ['dafunk.mp3'] }),
       },
 
       {
-        sound: new Howl({ src: ['encore.mp3']}),
-
+        sound: new Howl({ src: ['encore.mp3'] }),
       },
 
       {
-        sound: new Howl({ src: ['lucifer.mp3']}),
+        sound: new Howl({ src: ['lucifer.mp3'] }),
       },
     ];
 
     this.originalSounds = [
       {
-        sound: new Howl({ src: ['nfw.mp3']}),
-        volume: 0.7
+        sound: new Howl({ src: ['nfw.mp3'] }),
+        volume: 0.7,
       },
 
       {
-        sound: new Howl({ src: ['atm.mp3']}),
-        volume: 0.45
+        sound: new Howl({ src: ['atm.mp3'] }),
+        volume: 0.45,
       },
 
       {
-        sound: new Howl({ src: ['sigo.mp3']}),
-        volume: 0.8
+        sound: new Howl({ src: ['sigo.mp3'] }),
+        volume: 0.8,
       },
 
       {
-        sound: new Howl({ src: ['anthem.mp3']}),
-        volume: 0.9
+        sound: new Howl({ src: ['anthem.mp3'] }),
+        volume: 0.9,
       },
     ];
   }
 
-  handleVolumeChange(title, value)
-  {
-      let state = this.state;
-      let index = state.sliders.findIndex((slider) => slider.title === title);
-      state.sliders[index].volume = value;
+  handleVolumeChange(title, value) {
+    const state = this.state;
+    const index = state.sliders.findIndex(slider => slider.title === title);
+    state.sliders[index].volume = value;
 
-      if(this.state.toggle)
-        this.userSounds[index].sound.volume(value);
-      
-      this.setState(state);
+    if (this.state.toggle) this.userSounds[index].sound.volume(value);
+
+    this.setState(state);
   }
 
   hide() {
-      this.setState({ submitWindow: false });
+    this.setState({ submitWindow: false });
   }
 
-  handleSubmit(title, value)
-  {
-
-    this.userSounds.forEach((track, index) => {
+  handleSubmit(title, value) {
+    this.userSounds.forEach((track) => {
       track.sound.stop();
-    })
+    });
 
-    this.originalSounds.forEach(track => {
+    this.originalSounds.forEach((track) => {
       track.sound.stop();
-    })
+    });
 
     this.setState({ submitWindow: true });
   }
 
-  handleRestart(title, value)
-  {
-    this.setState(prevState => 
-    ({
-      sliders: prevState.sliders.map(slider => ({title: slider.title, volume: Math.random()})),
-      toggle: true
+  handleRestart(title, value) {
+    this.setState(prevState => ({
+      sliders: prevState.sliders.map(slider => ({
+        title: slider.title,
+        volume: Math.random(),
+      })),
+      toggle: true,
     }));
 
     this.userSounds.forEach((track, index) => {
       track.sound.seek(0);
       track.sound.volume(this.state.sliders[index].volume);
-    })
+    });
 
-    this.originalSounds.forEach(track => {
+    this.originalSounds.forEach((track) => {
       track.sound.seek(0);
       track.sound.volume(0);
-    })
+    });
   }
 
-  handleToggle(event)
-  {
-    this.setState({toggle: event.target.checked});
+  handleToggle(event) {
+    this.setState({ toggle: event.target.checked });
 
-    if(!event.target.checked){
-
-      this.userSounds.forEach((track, index) => {
+    if (!event.target.checked) {
+      this.userSounds.forEach((track) => {
         track.sound.volume(0);
-      })
+      });
 
-      this.originalSounds.forEach((track, index) => {
+      this.originalSounds.forEach((track) => {
         track.sound.volume(track.volume);
-      })
-    }
-
-    else {
+      });
+    } else {
       this.userSounds.forEach((track, index) => {
         track.sound.volume(this.state.sliders[index].volume);
-      })
+      });
 
-      this.originalSounds.forEach((track, index) => {
+      this.originalSounds.forEach((track) => {
         track.sound.volume(0);
-      })   
+      });
     }
-
   }
 
   render() {
-    let sliders = this.state.sliders.map((slider, index) => {
-      return (
-        <Slider data={slider} onVolumeChange={this.handleVolumeChange.bind(this)} key={index} />
-      )
-    });
+    const sliders = this.state.sliders.map((slider, index) => (
+        <Slider
+          data={slider}
+          onVolumeChange={this.handleVolumeChange.bind(this)}
+          key={index}
+        />
+    ));
     return (
       <div className="MixingDesk">
+        <div className="sliderContainer">
+          {sliders}
+        </div>
 
-        {sliders}
-        <label className="switch">
-          <input type="checkbox" checked={this.state.toggle} onChange={this.handleToggle.bind(this)}></input>
-          <span className="slider"></span>
-        </label>
+        <div className="controllsContainer">
+          <button type="button" onClick={this.handleSubmit.bind(this)}>
+            Submit
+          </button>
 
-        <button type="button" onClick={this.handleSubmit.bind(this)}>Submit</button>
+          <label className="switch">
+            <input
+              type="checkbox"
+              checked={this.state.toggle}
+              onChange={this.handleToggle.bind(this)}
+            />
+            <span className="slider" />
+          </label>
 
-        <button type="button" onClick={this.handleRestart.bind(this)}>Restart</button>
+          <button type="button" onClick={this.handleRestart.bind(this)}>
+            Restart
+          </button>
+        </div>
 
         <Rodal visible={this.state.submitWindow} onClose={this.hide.bind(this)}>
-          <div>
-            {this.calculateScore}
-          </div>
+          <div>{this.calculateScore}</div>
         </Rodal>
-
       </div>
     );
   }
 
   componentDidMount() {
-
     this.userSounds.forEach((track, index) => {
       track.sound.volume(this.state.sliders[index].volume);
       track.sound.play();
-    })
+    });
 
-    this.originalSounds.forEach(track => {
+    this.originalSounds.forEach((track) => {
       track.sound.volume(0);
       track.sound.play();
-    })
+    });
   }
 }
 
--- src/Slider.js
+++ src/Slider.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 
 class Slider extends Component {
-  
   handleChange(event) {
     this.props.onVolumeChange(this.props.data.title, event.target.value);
   }
 
   render() {
     return (
-    <div className="volbox" key={this.props.data.title}>
-    {this.props.data.title}<br/>
-    <input id="volume" type="range" orient="vertical" min="0" max="1" value={this.props.data.volume} step="0.01" onChange={this.handleChange.bind(this)}/>
-    </div>  
+      <div className="volbox" key={this.props.data.title}>
+        <input
+          id="volume"
+          type="range"
+          min="0"
+          max="1"
+          value={this.props.data.volume}
+          step="0.01"
+          onChange={this.handleChange.bind(this)}
+        />
+      </div>
     );
   }
 }
